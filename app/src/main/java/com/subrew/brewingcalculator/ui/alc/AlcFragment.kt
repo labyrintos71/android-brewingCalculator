c59e74a3b8acc3a7126dc8dd3c7d54f39f2517bb
Labyrintos <Labyrintos71@gmail.com>
2023-11-10 22:02:52 +0900

    알코올 도수계산 기능 추가

--- app/src/main/java/com/subrew/brewingcalculator/ui/alc/AlcFragment.kt
+++ app/src/main/java/com/subrew/brewingcalculator/ui/alc/AlcFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.subrew.brewingcalculator.databinding.FragmentAlcBinding
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class AlcFragment : Fragment() {
 
@@ -18,25 +21,48 @@ class AlcFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         val alcViewModel =
-                ViewModelProvider(this).get(AlcViewModel::class.java)
+            ViewModelProvider(this).get(AlcViewModel::class.java)
 
         _binding = FragmentAlcBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        binding.alcDefaultweight.setText("1231")
+        binding.alcFirstweight.setText("3606")
+        binding.alcNowweight.setText("0")
+        binding.alcDefaultweight.addTextChangedListener { calAlc() }
+        binding.alcFirstweight.addTextChangedListener { calAlc() }
+        binding.alcNowweight.addTextChangedListener { calAlc() }
 
-        val textView: TextView = binding.textGallery
-        alcViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textGallery
+//        alcViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun calAlc() {
+        val first = if (binding.alcFirstweight.text.toString()
+                .isNotEmpty()
+        ) binding.alcFirstweight.text.toString().toBigDecimal() else BigDecimal(3606)
+        val now = if (binding.alcNowweight.text.toString()
+                .isNotEmpty()
+        ) binding.alcNowweight.text.toString().toBigDecimal() else BigDecimal(0)
+        val def = if (binding.alcDefaultweight.text.toString()
+                .isNotEmpty()
+        ) binding.alcDefaultweight.text.toString().toBigDecimal() else BigDecimal(1231)
+
+
+        binding.alcResult.text = "${first.minus(now).divide(now.minus(def),3, RoundingMode.HALF_EVEN)}%"
+
+        // 알코올 도수 = (처음 총무게 - 현재 총 무게)/(현재 총무게 - 용기의 무게)
     }
 }
